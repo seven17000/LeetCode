fc03baccfa6b84b663ec0ecd3c8bf4f5a57ee592
Seven <seven17000@163.com>
2017-04-13 13:00:50 +0800

    递归和非递归分别实现二叉树的前序遍历

---
+++ preOrderTraversal.cpp
@@ -0,0 +1,47 @@
+/*
+*问题：给出一颗二叉树，返回其结点的前序遍历，尝试使用非递归解决。
+*答者：Seven17000
+*/
+
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+vector<int> result;
+vector<int> preorderTraversal(TreeNode* root)
+{//递归解决
+	if (root == NULL)
+		return result;
+
+	if (root)
+	{
+		result.push_back(root->val);
+		preorderTraversal(root->left);
+		preorderTraversal(root->right);
+	}
+	return result;
+}
+
+vector<int> preorderTraversal(TreeNode* root)
+{//非递归
+	vector<int> result;
+	stack<TreeNode*> s;
+
+	if (root == NULL)
+		return vector;
+
+	while (root || s.empty())
+	{
+		while (root)
+		{
+			s.push(root);
+			result.push_back(root->val);
+			root->left;
+		}
+		root = s.top();
+		s.pop();
+		root = root->right;
+	}
+	return result;
+}
